7f2ad4b9f9c06482ca275493fa0641589c74fae8
DuoAI-H <lfpintoo@gmail.com>
2020-12-29 02:28:28 -0500

    Primeros Ejercicios

---
+++ Ejercicios/04Defer/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func first()  {
+	fmt.Println("1st")
+}
+
+func second()  {
+	fmt.Println("2nd")
+}
+
+func main() {
+	//Programa a la funcion para ser llamada
+	//cuando la funcion que lo contiene, complete.
+	defer second()
+	first()
+}
