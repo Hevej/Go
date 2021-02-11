31e361454560c6623ec331f9cb04be62bc592d7d
DuoAI-H <lfpintoo@gmail.com>
2021-02-11 16:10:44 -0500

    0.4.4

--- Book_MasteringGo/04 Enhace Go Code with Data Structures/02 Hash Tables/YT/makingHash.go
+++ Book_MasteringGo/04 Enhace Go Code with Data Structures/02 Hash Tables/YT/makingHash.go
@@ -21,14 +21,23 @@ type bucketNode struct {
 
 // Init will create a bucket in each slot of the hash table
 func Init() *HashTable {
+	//Crea una hashtable
+	//Lo localizo por que tengo que devolver un puntero
 	result := &HashTable{}
+	//Navego entre la estructura interna de result, un array de longitud 7
 	for i := range result.array {
+		//A cada elemento le asigno un puntero de bucket{}, el tipo que espera
+		//Al iniciarlizarlo asi, quedara cada bucket guardando una dirección y no
+		//quedara el array nulo
 		result.array[i] = &bucket{}
 	}
+	//Devuelvo la hash table ya hecha
 	return result
 }
 
 func main() {
+	//Retorna una hash table donde cada slot tiene un bucket
 	testHashTable := Init()
 	fmt.Println(testHashTable)
+	fmt.Println("Estado incompleto hasta aprender recievers")
 }
