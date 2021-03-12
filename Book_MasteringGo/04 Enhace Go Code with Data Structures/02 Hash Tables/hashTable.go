2184b738fae78eaf7ca5d8179f4a8ecc87ace804
Luis Felipe Pinto Ortegón <lfpintoo@gmail.com>
2021-03-11 23:40:56 -0500

    Electron
    Agregando Tutorial de Electron

--- Book_MasteringGo/04 Enhace Go Code with Data Structures/02 Hash Tables/hashTable.go
+++ Book_MasteringGo/04 Enhace Go Code with Data Structures/02 Hash Tables/hashTable.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// Definition of the node
 const SIZE = 15
 
 type Node struct {
@@ -19,18 +20,22 @@ func hashFunction(i, size int) int {
 }
 
 func insert(hash *HashTable, value int) int {
+	//Crea un identificador para el valor en la hashtable
 	index := hashFunction(value, hash.Size)
+	//Crea un nodo con el valor
 	element := Node{Value: value, Next: hash.Table[index]}
+	//Lo adiciona a la hash table
 	hash.Table[index] = &element
 	return index
 }
 
+//Solo imprime la tabla
 func traverse(hash *HashTable) {
 	for k := range hash.Table {
 		if hash.Table[k] != nil {
 			t := hash.Table[k]
 			for t != nil {
-				fmt.Println("%d -> ", t.Value)
+				fmt.Printf("%d -> ", t.Value)
 				t = t.Next
 			}
 			fmt.Println()
@@ -38,12 +43,39 @@ func traverse(hash *HashTable) {
 	}
 }
 
+//Determina en donde esta un elemento
+func lookup(hash *HashTable, value int) bool {
+	//Obtengo el identificador
+	index := hashFunction(value, hash.Size)
+	//Si el identificador tiene valor
+	if hash.Table[index] != nil {
+		//Obtengo la linked list
+		t := hash.Table[index]
+		for t != nil {
+			if t.Value == value {
+				return true
+			}
+			//Aca me va cambiando los valores para revisar el siguiente
+			t = t.Next
+		}
+	}
+	return false
+}
+
 func main() {
+	//Creo la estructura - backbone
 	table := make(map[int]*Node, SIZE)
+	//Creo la hastable y retorno un address
 	hash := &HashTable{Table: table, Size: SIZE}
 	fmt.Println("Number of spaces:", hash.Size)
+	//Inserto valores del 0 al 119
 	for i := 0; i < 120; i++ {
 		insert(hash, i)
 	}
 	traverse(hash)
+
+
+	fmt.Println(lookup(hash, 20))
+	fmt.Println(lookup(hash, 220))
+
 }
